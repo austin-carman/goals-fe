13c3c3c96ca9a63458b807765b797ef4e5e53606
Austin Carman <abcarman12@gmail.com>
2021-10-19 09:57:35 -1000

    Route for profile. Basic structure for profile

---
+++ src/components/Profile.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div>
+      <h2>Welcome Back {/* insert user here */}!</h2>
+      <button>New Goal</button>
+    </div>
+  );
+};
+
+export default Profile;
