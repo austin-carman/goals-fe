39803016c5b1f8eba43e8fa6bbec0a6b92781ffc
Austin Carman <abcarman12@gmail.com>
2021-10-19 09:14:22 -1000

    added classnames to nav links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,11 +6,19 @@ const Navbar = () => {
     <header className="navbar-header">
       <nav className="navbar">
         <h2>Goal Tracker</h2>
-        <div className="nav-links-container">
-          <Link to="/register">Create Account</Link>
-          <Link to="/login">Sign In</Link>
-          <Link to="/profile">Profile</Link>
-          <Link to="/">Home</Link>
+        <div className="links-container">
+          <Link to="/register" className="navlink">
+            Create Account
+          </Link>
+          <Link to="/login" className="navlink">
+            Sign In
+          </Link>
+          <Link to="/profile" className="navlink">
+            Profile
+          </Link>
+          <Link to="/" className="navlink">
+            Home
+          </Link>
         </div>
       </nav>
     </header>
