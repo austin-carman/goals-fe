e73bc514f996b18f11e1a6d2a14559c96762203b
Austin Carman <abcarman12@gmail.com>
2021-10-21 11:32:33 -1000

    GoalList created and rendering. Need to flesh out

---
+++ src/components/GoalList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const GoalList = () => {
+  return (
+    <div>
+      <h2>Goal List</h2>
+    </div>
+  );
+};
+
+export default GoalList;
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,13 @@
 import React from "react";
+import GoalList from "./GoalList";
 
 const Profile = () => {
   return (
     <div>
+      <h2>Profile Page</h2>
+      <p>Info about user/profile</p>
       <button>New Goal</button>
+      <GoalList />
     </div>
   );
 };
