62a34ab470390d75c49978fe4839a3228d2a77a7
Austin Carman <abcarman12@gmail.com>
2021-10-21 11:34:46 -1000

    axiosWithAuth .get working, need to set to state

--- src/components/GoalList.js
+++ src/components/GoalList.js
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const GoalList = () => {
+  const [goals, setGoals] = useState([]); //eslint-disable-line
+  const location = useLocation();
+  const { userId } = location.state;
+  console.log(goals);
+  console.log(userId);
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get(`https://goalmanager.herokuapp.com/api/goals/${userId}`)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div>
       <h2>Goal List</h2>
