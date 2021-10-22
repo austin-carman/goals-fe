51d6145c01968eb9aaa2cbe134e9f36226e03fd8
Austin Carman <abcarman12@gmail.com>
2021-10-22 09:52:17 -1000

    form inputs built out

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,9 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Register = () => {
+  const initialState = {
+    first_name: "",
+    last_name: "",
+    username: "",
+    password: "",
+  };
+  // eslint-disable-next-line no-unused-vars
+  const [registerForm, setRegisterForm] = useState(initialState);
+
+  const handleChange = () => {
+    console.log("handleChange");
+  };
+
+  const handleSubmit = () => {
+    console.log("handleSubmit");
+  };
+
   return (
     <div>
-      <h2>Register</h2>
+      <h2>Create Account</h2>
+      <input
+        type="text"
+        name="first_name"
+        value={registerForm.first_name}
+        onChange={handleChange}
+        placeholder="First name"
+      />
+      <input
+        type="text"
+        name="last_name"
+        value={registerForm.last_name}
+        onChange={handleChange}
+        placeholder="Last name"
+      />
+      <input
+        type="text"
+        name="username"
+        value={registerForm.username}
+        onChange={handleChange}
+        placeholder="username"
+      />
+      <input
+        type="text"
+        name="password"
+        value={registerForm.password}
+        onChange={handleChange}
+        placeholder="password"
+      />
+      <button onClick={handleSubmit}>Sign-In</button>
     </div>
   );
 };
