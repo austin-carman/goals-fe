e6ca7f6d401d29eee08700f1125e2b4e135f7159
Austin Carman <abcarman12@gmail.com>
2021-10-25 11:17:23 -1000

    added more structure to new goal form

--- src/components/NewGoal.js
+++ src/components/NewGoal.js
@@ -1,9 +1,36 @@
-import React from "react";
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
 
 const NewGoal = () => {
+  const initialState = {
+    goal_title: "",
+  };
+  const [newGoal, setNewGoal] = useState(initialState);
+
+  const handleChange = (e) => {
+    console.log("change");
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(newGoal);
+  };
+
   return (
     <div>
       <h2>New Goal</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Goal Title:
+          <input
+            type="text"
+            name="goal_title"
+            value={newGoal.goal_title}
+            onChange={handleChange}
+          />
+        </label>
+      </form>
+      <button>Create Goal</button>
     </div>
   );
 };
