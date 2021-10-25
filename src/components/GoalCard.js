9c72dcfe0a296e35848ee1e373d58d3ec8ca5229
Austin Carman <abcarman12@gmail.com>
2021-10-25 10:12:11 -1000

    jsx. added step notes, #, conditional className

--- src/components/GoalCard.js
+++ src/components/GoalCard.js
@@ -3,12 +3,23 @@ import PropTypes from "prop-types";
 
 const GoalCard = (props) => {
   const { goal } = props;
+  let stepCount = 0;
 
   return (
-    <div>
+    <div className="active-goals">
       <h2>{goal.goal_title}</h2>
       {goal.steps.map((step) => {
-        return <p key={step.step_id}>{step.step_title}</p>;
+        const goalStatus =
+          step.step_completed === true ? "completed-goal" : "active-goal";
+        stepCount += 1;
+        return (
+          <div key={step.step_id} className={goalStatus}>
+            <h3>
+              Step {stepCount}: {step.step_title}
+            </h3>
+            {step.step_notes ? <p>Notes: {step.step_notes}</p> : null}
+          </div>
+        );
       })}
     </div>
   );
