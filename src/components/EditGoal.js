6a5bad1ca5ac5c92803e7aec9a497c8ddb5655f6
Austin Carman <abcarman12@gmail.com>
2021-10-26 12:58:57 -1000

    set up state, goal title changes with user input

--- src/components/EditGoal.js
+++ src/components/EditGoal.js
@@ -1,13 +1,36 @@
-import React from "react";
-import { useParams, useLocation } from "react-router-dom";
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
+
 const EditGoal = () => {
-  const params = useParams();
   const location = useLocation();
+  const { goal_title, steps } = location.state.goal;
+  const initialState = {
+    goal_title: goal_title,
+    steps: steps,
+  };
+  const [goal, setGoal] = useState(initialState);
+
+  const handleChange = (evt) => {
+    const { name, value } = evt.target;
+    setGoal({ ...goal, [name]: value });
+  };
 
   return (
     <div>
-      <h2>Edit Goal {params.goalId}</h2>
-      <p>{location.state.goal.goal_title}</p>
+      <form>
+        <label>
+          Goal Title
+          <input
+            type="text"
+            name="goal_title"
+            value={goal.goal_title}
+            onChange={handleChange}
+          />
+        </label>
+      </form>
+      <button>Save</button>
+      <button>Cancel</button>
+      <button>Delete</button>
     </div>
   );
 };
