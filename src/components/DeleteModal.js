058fa5c909114a052e979e74eb5a92eeffd159bc
Austin Carman <abcarman12@gmail.com>
2021-10-27 10:25:14 -1000

    basic modal styles. Need to update styles more

--- src/components/DeleteModal.js
+++ src/components/DeleteModal.js
@@ -2,6 +2,17 @@ import React from "react";
 import Modal from "react-modal";
 import PropTypes from "prop-types";
 
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
 Modal.setAppElement("#root");
 
 const DeleteModal = ({ isModalOpen, setIsModalOpen }) => {
@@ -16,7 +27,11 @@ const DeleteModal = ({ isModalOpen, setIsModalOpen }) => {
 
   return (
     <div>
-      <Modal isOpen={isModalOpen} onRequestClose={closeModal}>
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+      >
         <h2>Are you sure you want to delete your goal?</h2>
         <button onClick={closeModal}>Cancel</button>
         <button onClick={handleDelete}>Delete</button>
