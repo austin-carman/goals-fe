2319cb0e7aafeb2a4cf6a64e998db40400b6174d
Austin Carman <abcarman12@gmail.com>
2021-11-04 10:17:31 -1000

    set up initial state for reducer

--- src/reducers/goalReducer.js
+++ src/reducers/goalReducer.js
@@ -1,5 +1,25 @@
-const goalReducer = () => {
-  console.log("goal reducer");
+const initialState = [
+  {
+    goal_id: null,
+    user_id: null,
+    goal_title: "",
+    goal_completed: null,
+    steps: [
+      {
+        step_id: null,
+        goal_id: null,
+        step_title: "",
+        step_notes: "",
+        step_completed: null,
+      },
+    ],
+  },
+];
+const goalReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
 };
 
 export default goalReducer;
