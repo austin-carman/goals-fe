e81fb64fd4c7ce7e85340f4728d7c1ebe2490808
Austin Carman <abcarman12@gmail.com>
2021-11-15 11:38:27 -1000

    last name schema

--- src/schema/signUpSchema.js
+++ src/schema/signUpSchema.js
@@ -2,6 +2,7 @@ import * as yup from "yup";
 
 const signUpSchema = yup.object().shape({
   first_name: yup.string().trim().required("First name is required field"),
+  last_name: yup.string().trim().required("Last name is required field"),
 });
 
 export default signUpSchema;
