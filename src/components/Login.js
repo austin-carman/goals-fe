508da14c4275c6b196c5a782f5cb3b80295ef126
Austin Carman <abcarman12@gmail.com>
2021-11-15 09:27:01 -1000

    removed old code

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,9 +11,9 @@ const Login = (props) => {
   };
 
   const [loginForm, setLoginForm] = useState(initialState);
-  // const [errMessage, setErrMessage] = useState();
-  // const [isLoading, setIsLoading] = useState(false); // change to isFetching from app state
   const history = useHistory();
+
+  console.log("props:", props);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -25,32 +25,14 @@ const Login = (props) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // setIsLoading(true); // isFetching
     props.userLogin(loginForm);
     localStorage.getItem("token") && history.push(`/profile/${props.userId}`);
-
-    // axios
-    //   .post("https://goalmanager.herokuapp.com/api/user/login", loginForm)
-    //   .then((res) => {
-    //     if (!res.data.token) {
-    //       setErrMessage("Invalid username or password");
-    //       // setIsFetching(false);
-    //     } else {
-    //       setErrMessage();
-    //       localStorage.setItem("token", res.data.token);
-    //       setIsLoading(false); // setIsFetching(false);
-    // push(`/profile/${res.data.userId}`);
-    //     }
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   });
     setLoginForm(initialState);
   };
 
   return (
     <div>
-      <h2>Sign-In</h2>
+      <h2>Sign In</h2>
       <input
         type="text"
         name="username"
@@ -65,9 +47,7 @@ const Login = (props) => {
         onChange={handleChange}
         placeholder="password"
       />
-      <button onClick={handleSubmit}>Sign-In</button>
-      {/* {errMessage ? <p>{errMessage}</p> : null} */}
-      {/* {isLoading ? <h3>Loading...</h3> : null} isFetching */}
+      <button onClick={handleSubmit}>Sign In</button>
     </div>
   );
 };
