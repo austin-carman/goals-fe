2f2bec8d79b4a7f40f108c5e8264e0f83015e287
Austin Carman <abcarman12@gmail.com>
2021-12-01 11:28:55 -1000

    Remove unnecessary code from Profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -7,8 +7,6 @@ const Profile = () => {
 
   return (
     <div>
-      <h2>Profile Page</h2>
-      <p>Info about user/profile</p>
       <Link to={`/new-goal/${userId}`}>
         <button>New Goal</button>
       </Link>
