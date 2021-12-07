6b5b1cef819689c1ace226fd727de00c4c2f51dc
Austin Carman <abcarman12@gmail.com>
2021-12-07 11:08:14 -1000

    added quotes and updated paragraph info on home

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,27 +12,43 @@ const Home = () => {
   return (
     <div>
       <section className="hero">
-        <h1>Goal Tracker</h1>
-        <button onClick={handleClick}>Track Your Goals</button>
+        <h1>Where will goals take you?</h1>
+        <button onClick={handleClick}>Start now</button>
       </section>
-      <section className="top-info">
-        <h2>Remember your goals</h2>
+      <section className="info-section">
+        <div>
+          <h2>&quot;A goal properly set is halfway reached.&quot;</h2>
+          <h4>— Zig Ziglar</h4>
+        </div>
         <p>
-          Haven&apos; t we all made a goal only to forget about it later?With
-          GoalTracker all your goals are grouped together and easily accessible
-          so you won&apos; t be stuck trying to remember them down the road.
+          We&apos;ve all made a goal that we end up forgetting about in our busy
+          lives. With GoalTracker all your goals are grouped together in one
+          convenient location so you can easily view them without having to try
+          and remember everything.
         </p>
       </section>
-      <section className="middle-info">
-        <h2>Break your goal down</h2>
+      <section className="info-section">
+        <div>
+          <h2>
+            &quot;Goals are pure fantasy unless you have a specific plan to
+            achieve them.&quot;
+          </h2>
+          <h4> — Stephen Covey</h4>
+        </div>
         <p>
           GoalTracker allows you to create a goal with steps, or smaller
           landmark goals, to help you stay organized and make achieving your
           goal more manageable.
         </p>
       </section>
-      <section className="bottom-info">
-        <h2>Record your progress</h2>
+      <section className="info-section">
+        <div>
+          <h2>
+            &quot;What you get by achieving your goals is not as important as
+            what you become by achieving your goals.&quot;
+          </h2>
+          <h4>— Zig Ziglar</h4>
+        </div>
         <p>
           With each step you can record notes to record your progress or leave
           reminders for your future self. Once you achieve a step, mark it as
