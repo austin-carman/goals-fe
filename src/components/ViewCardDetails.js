dff5c8740cd56b02191f27cba51af98c83a419af
Austin Carman <abcarman12@gmail.com>
2021-12-09 10:40:52 -1000

    ViewCardDetaisl styling

--- src/components/ViewCardDetails.js
+++ src/components/ViewCardDetails.js
@@ -1,10 +1,10 @@
-/* eslint-disable no-unused-vars */
 import React, { useState } from "react";
 import Modal from "react-modal";
 import PropTypes from "prop-types";
 import { useHistory } from "react-router";
 import { editUserGoal } from "../actions/goalsActions";
 import { connect } from "react-redux";
+import closeIcon from "../images/close-icon.png";
 
 const customStyles = {
   content: {
@@ -14,7 +14,8 @@ const customStyles = {
     bottom: "auto",
     marginRight: "-50%",
     transform: "translate(-50%, -50%)",
-    width: "75%",
+    height: "75vh",
+    width: "50%",
   },
 };
 
@@ -50,39 +51,42 @@ const ViewCardDetails = (props) => {
   };
 
   return (
-    <div>
+    <div className="view-details-modal">
       <Modal
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
         style={customStyles}
       >
-        <button onClick={closeModal}>Close Modal</button>
-        <input
-          className="completed-checkbox"
-          type="checkbox"
-          name="goal_completed"
-          value={goalDetails.goal_completed}
-          checked={goalDetails.goal_completed}
-          onChange={(e) => handleChange(e)}
-        />
-        <h2>{goalDetails.goal_title}</h2>
-        <div className="goal-card-header-container"></div>
-        <button
-          className="goal-card-edit-button"
-          onClick={() => handleEdit(goalIndex)}
-        >
-          Edit
-        </button>
+        <div className="modal-actions-container">
+          <img src={closeIcon} className="close-details" onClick={closeModal} />
+          <button
+            className="goal-card-edit-button"
+            onClick={() => handleEdit(goalIndex)}
+          >
+            Edit
+          </button>
+        </div>
+        <div className="details-container">
+          <input
+            className="completed-checkbox"
+            type="checkbox"
+            name="goal_completed"
+            value={goalDetails.goal_completed}
+            checked={goalDetails.goal_completed}
+            onChange={(e) => handleChange(e)}
+          />
+          <h2>{goalDetails.goal_title}</h2>
+        </div>
         {goalDetails.steps.map((step, index) => {
           stepNumber += 1;
           return (
             <div
-              key={step.step_id}
               className={
                 step.step_completed
-                  ? "goal-card-completed-steps goal-card-step-container"
-                  : "goal-card-unfinished-steps goal-card-step-container"
+                  ? "completed-steps details-container"
+                  : "unfinished-steps details-container"
               }
+              key={step.step_id}
             >
               <input
                 className="completed-checkbox"
@@ -92,13 +96,17 @@ const ViewCardDetails = (props) => {
                 checked={step.step_completed}
                 onChange={(e) => handleChange(e, index)}
               />
-              <div className="goal-card-step-title-container">
-                <h3 className="goal-card-step-number">Step {stepNumber}:</h3>
-                <h3 className="goal-card-step-title">{step.step_title}</h3>
+              <div className="checkbox-title-container">
+                <h3 className="view-goal-step-number">Step {stepNumber}: </h3>
+                <h3 className="view-goal-step-title">{step.step_title}</h3>
+                {step.step_notes && (
+                  <div className="step-info-container">
+                    <p className="view-goal-step-notes">
+                      Notes: {step.step_notes}
+                    </p>
+                  </div>
+                )}
               </div>
-              {step.step_notes ? (
-                <p className="goal-card-step-notes">Notes: {step.step_notes}</p>
-              ) : null}
             </div>
           );
         })}
