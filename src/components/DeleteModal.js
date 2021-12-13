846ac9b296367ae4ba0a8649f01544a0d4c55448
Austin Carman <abcarman12@gmail.com>
2021-12-13 09:57:06 -1000

    modal styles

--- src/components/DeleteModal.js
+++ src/components/DeleteModal.js
@@ -12,21 +12,6 @@ Modal.setAppElement("#root");
 const DeleteModal = (props) => {
   const { isModalOpen, setIsModalOpen, deleteGoal, deleteStep, goal, setGoal } =
     props;
-  // const customStyles = {
-  //   content: {
-  //     top: "50%",
-  //     left: "50%",
-  //     right: "auto",
-  //     bottom: "auto",
-  //     marginRight: "-50%",
-  //     transform: "translate(-50%, -50%)",
-  //     borderRadius: "10px",
-  //     border: "none",
-  //   },
-  //   overlay: {
-  //     backdropFilter: "blur(3px) brightness(40%)",
-  //   },
-  // };
   const history = useHistory();
 
   const closeModal = () => {
