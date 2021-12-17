6fc13274b8970e455168f2d716299442d4c8d9e7
Austin Carman <abcarman12@gmail.com>
2021-12-16 16:44:36 -1000

    navbar media queries

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,63 +29,62 @@ const Navbar = (props) => {
   };
 
   return (
-    <header className="navbar-header">
-      <nav className="navbar">
-        <Link to="/" className="home-link">
-          <h2>Goal Tracker</h2>
-        </Link>
-        {props.userId ? (
-          <div className="links-container">
-            <div className="navlink profile-avatar-container">
-              <h3 className="avatar-text">{initials}</h3>
-            </div>
-            <div className="profile-menu-options">
-              <Link
-                to={`/profile/${props.userId}`}
-                className="navlink logout-navlink"
-              >
-                Profile
-              </Link>
-              <Link
-                onClick={handleSignOut}
-                to="/"
-                className="navlink logout-navlink"
-              >
-                Logout
-              </Link>
-              <div className="background-selection-container">
-                <p>Profile Background:</p>
-                <select
-                  value={props.backgroundImage}
-                  onChange={handleBackground}
-                >
-                  <option value={ocean1}>Ocean Sunrise</option>
-                  <option value={ocean2}>Tropical Shoreline</option>
-                  <option value={oceanMountains}>Tropical Mountains</option>
-                  <option value={mountains1}>Mountain Sunset</option>
-                  <option value={mountainHaze}>Mountain Haze</option>
-                  <option value={desert}>Desert</option>
-                  <option value={fall}>Fall</option>
-                  <option value={snow}>Snow</option>
-                  <option value={plants}>Plants</option>
-                  <option value={balloons}>Balloons</option>
-                  <option value={rockClimber}>Rock Climber</option>
-                </select>
-              </div>
+    <nav className="navbar">
+      <Link to="/" className="home-link">
+        <h2>Goal Tracker</h2>
+      </Link>
+      {props.userId ? (
+        <div className="links-container">
+          <div className="profile-avatar-container">
+            <h3 className="avatar-text">{initials}</h3>
+          </div>
+          <div className="profile-menu-options">
+            <Link
+              to={`/profile/${props.userId}`}
+              className="navlink logout-navlink"
+            >
+              Profile
+            </Link>
+            <Link
+              onClick={handleSignOut}
+              to="/"
+              className="navlink logout-navlink"
+            >
+              Logout
+            </Link>
+            <div className="navlink background-selection-container">
+              <h6>Background:</h6>
+              <select value={props.backgroundImage} onChange={handleBackground}>
+                <option value={ocean1}>Ocean Sunrise</option>
+                <option value={ocean2}>Tropical Shoreline</option>
+                <option value={oceanMountains}>Tropical Mountains</option>
+                <option value={mountains1}>Mountain Sunset</option>
+                <option value={mountainHaze}>Mountain Haze</option>
+                <option value={desert}>Desert</option>
+                <option value={fall}>Fall</option>
+                <option value={snow}>Snow</option>
+                <option value={plants}>Plants</option>
+                <option value={balloons}>Balloons</option>
+                <option value={rockClimber}>Rock Climber</option>
+              </select>
             </div>
           </div>
-        ) : (
-          <div id="signed-out-links-container">
-            <Link to="/login" className="navlink">
-              Sign In
-            </Link>
-            <Link to="/register" className="navlink" id="sign-up-link">
-              Sign Up
-            </Link>
-          </div>
-        )}
-      </nav>
-    </header>
+        </div>
+      ) : (
+        <div id="signed-out-links-container">
+          <Link to="/login" className="navlink signed-out-links">
+            Sign In
+          </Link>
+          <Link
+            to="/register"
+            className="navlink signed-out-links"
+            id="sign-up-link"
+          >
+            Sign Up
+          </Link>
+        </div>
+      )}
+    </nav>
   );
 };
 
